464bb7aba2b73d9abf0c714bd910a37de29007bd
barat <barathalac@gmail.com>
2025-12-25 13:48:35 +0300

    remove swcMinify option from Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* Optimization config */
-  swcMinify: true,
   poweredByHeader: false,
   compress: true,
   productionBrowserSourceMaps: false,
